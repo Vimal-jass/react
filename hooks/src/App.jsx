bc8b167dac93a0a7da534ef4273f113b42de1a0a
jass-vimal <vneeclasses@gmail.com>
2025-10-15 17:13:12 +0530

    Updated project with latest changes

---
+++ hooks/src/App.jsx
@@ -0,0 +1,19 @@
+import {useState }from 'react'
+
+
+
+const App = () => {
+  
+
+
+  return (
+
+    <>
+<h1>your number is {first}</h1>
+<button>click</button>
+    </>
+   
+  )
+}
+
+export default App
--- sheryians-coding-school/src/components/Card.jsx
+++ sheryians-coding-school/src/components/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({jass}) => {
 {jass.map((card, index)=>{
 return (
     <div key={index} className="w-[300px] m-10 bg-white rounded-xl shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300 ">
-  <img src={card.image} alt="Card Image" className="w-full h-48 object-cover" />
+  <img src={card.thumbnail} alt="Card Image" className="w-full h-48 object-cover" />
   <div className="p-6">
     <h2 className="text-xl font-bold text-gray-800 mb-2">{card.title}</h2>
     <p className="text-gray-600 text-sm mb-2">{card.description}</p>
